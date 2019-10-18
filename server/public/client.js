6b4d0f88b377cf18570cf391bcd00fe2929fb906
Iris Noh <irisnoh22@gmail.com>
2019-10-18 17:11:21 -0500

    show table on DOM, show completed button only for false

--- server/public/client.js
+++ server/public/client.js
@@ -2,11 +2,42 @@ console.log('in javascript');
 
 $('document').ready(onReady);
 
-function onReady () {
-$('#submitButton').on('click', onSubmit)
-console.log('in jquery');
+function onReady() {
+    $('#submitButton').on('click', onSubmit);
+    getTask();
+
+    console.log('in jquery');
 }
 
-function onSubmit () {
-    console.log ('submit was clicked');
+
+function onSubmit() {
+    getTask();
+    console.log('submit was clicked');
 }
+function getTask() { // GET what is in my response from server to DOM
+    console.log('in getTask');
+    $.ajax({
+        type: 'GET',
+        url: '/task',
+    }).then(function (response) {
+        appendTask(response);
+    }).catch(function (error) {
+        console.log('error in get', error);
+    })
+
+} // end getTask
+
+
+  function appendTask (taskArray) {
+    $('#viewTask').empty();
+    taskArray.forEach(function(task){
+      let readyButton = '';
+      console.log(task);
+      if (task.completed == false) {
+        readyButton =`<button data-id="${task.id}" id="completedButton">Completed?</button>`
+      }
+      stringToAppend = `<tr><td>${task.task}</td><td>${task.completed}</td>><td>${readyButton}</td>
+      <td><button id = "Delete"> Delete</button></td></tr>`;
+      $('#viewTask').append(stringToAppend);
+    })
+  }
