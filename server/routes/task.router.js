9bc7c18f6f19f95f821bb137db5a10dc6826cfe0
Iris Noh <irisnoh22@gmail.com>
2019-10-18 16:39:54 -0500

    have POST work through postman

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -5,7 +5,7 @@ const taskRouter = express.Router();
 const pg = require('pg');
 const Pool = pg.Pool;
 const config = {
-    database: 'koala_holla',
+    database: 'weekend-to-do-app',
     host: 'localhost',
     port: 5432,
     max: 10,
@@ -22,10 +22,32 @@ pool.on('error', (error) => {
     console.log('Error connecting to db', error);
 })
 
+// GET
+taskRouter.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "to-do-list";';
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    })
+        .catch(error => {
+            console.log('error getting tasks', error);
+            res.sendStatus(500);
+        });
+});
 
-
-
-
+// POST
+taskRouter.post('/', (req, res) => {
+    let newTask = req.body
+    console.log(`Adding koala`, newTask);
+    let queryText = `INSERT INTO "to-do-list" ("task", "completed") VALUES ($1, $2);`;
+    pool.query(queryText, [newTask.task, newTask.completed])
+        .then(result => {
+            res.sendStatus(200);
+        })
+        .catch(error => {
+            console.log(`Error adding new task`, error);
+            res.sendStatus(500);
+        });
+});
 
 
 
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const taskRouter = require('./routes/task.router')
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
