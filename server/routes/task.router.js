95dd06532ef4a672e9634065c52566b12a7a74ae
Iris Noh <irisnoh22@gmail.com>
2019-10-21 11:24:29 -0500

    change pool folder

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const taskRouter = express.Router();
 
-// DB CONNECTION
-const pg = require('pg');
-const Pool = pg.Pool;
-const config = {
-    database: 'weekend-to-do-app',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 10000
-}
+// // DB CONNECTION
+// const pg = require('pg');
+// const Pool = pg.Pool;
+// const config = {
+//     database: 'weekend-to-do-app',
+//     host: 'localhost',
+//     port: 5432,
+//     max: 10,
+//     idleTimeoutMillis: 10000
+// }
 
-const pool = new Pool(config);
+// const pool = new Pool(config);
 
-pool.on('connect', () => {
-    console.log('postgresql connected!!');
-});
+// pool.on('connect', () => {
+//     console.log('postgresql connected!!');
+// });
 
-pool.on('error', (error) => {
-    console.log('Error connecting to db', error);
-})
+// pool.on('error', (error) => {
+//     console.log('Error connecting to db', error);
+// })
 
 // GET
 taskRouter.get('/', (req, res) => {
